47170dd8dcf6523e72a9d023b07e446e3d25fa13
Daniel Arpide <djarpide@gmail.com>
2022-04-26 12:51:23 -0400

    website commit

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--d_m6#gr_(k4)=b+)zl1ecr^gqqa)=%%s-q78f8xcwxqt0_ta4'
+from dotenv import load_dotenv
+load_dotenv()
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
