6f96c47cd316aa3fd284ff6ca376aaedb2253164
Daniel Arpide <djarpide@gmail.com>
2022-04-26 21:00:46 -0400

    fixed security problem

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -20,11 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+from decouple import config
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--d_m6#gr_(k4)=b+)zl1ecr^gqqa)=%%s-q78f8xcwxqt0_ta4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = [
     '173.230.133.146',
