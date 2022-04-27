588311c81eb8e9e4b955498e25a7e03abb3fbad1
Daniel Arpide <djarpide@gmail.com>
2022-04-27 00:08:04 -0400

    fixing security

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import json
 from pathlib import Path
+
+with open('../config.json') as config_file:
+    config = json.load(config_file)
 
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-from decouple import config
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = config['DEBUG']
 
 ALLOWED_HOSTS = [
     '173.230.133.146',
