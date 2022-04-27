ef9f27fb417dc1994723cb74f75979e197f62aa1
Daniel Arpide <djarpide@gmail.com>
2022-04-27 01:00:48 -0400

    fixing security

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -32,10 +32,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure--d_m6#gr_(k4)=b+)zl1ecr^gqqa)=%%s-q78f8xcwxqt0_ta4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
