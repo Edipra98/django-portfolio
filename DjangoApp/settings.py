16232a6bb837a1441c9651a46be9886d93400126
Daniel Arpide <djarpide@gmail.com>
2022-04-27 01:01:58 -0400

    fixing security

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--d_m6#gr_(k4)=b+)zl1ecr^gqqa)=%%s-q78f8xcwxqt0_ta4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
